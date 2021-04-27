9c0ca95ab18d7c477df1003774069b4e32d10fe5
Sean Moulton <sean.moulton@cargomatrix.com>
2021-04-27 10:39:23 -0400

    added Table script

---
+++ Create a new Table.sql
@@ -0,0 +1,15 @@
+use CMX_Accounting
+
+CREATE TABLE CMX_Product_Users_COM
+ (
+    [Id] NVARCHAR(100),
+    [System] NVARCHAR(100),
+    [FirstName] NVARCHAR(100),
+    [LastName] NVARCHAR(100),
+    [UserEmail] NVARCHAR(100),
+    [Station] NVARCHAR(100),
+    [LastLogin] NVARCHAR(100),
+    [DisabledOn] NVARCHAR(100),
+    [Comment] NVARCHAR(100),
+    [DateCreated] NVARCHAR(100)
+      );
